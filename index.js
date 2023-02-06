f2d1aaa286c9fbf4f6ac836cc8ff4f4234b029a4
Victor Meneghini <victormeneghini@pop-os.localdomain>
2023-02-06 21:44:57 +0000

    feat: extract methods from request

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 // Dependencies
 const http = require("http")
+const url = require("url")
 
+// Server
 const server = http.createServer((req, res) => {
-  console.log(req)
 
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true)
 
-  res.statusCode = 200
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "")
+
+  // Get HTTP method
+  const method = req.method.toLowerCase()
+  console.log(method)
+
+  // Send the response
   res.end("hello world")
+
+
+  // Log the user request path
+
 })
 
+
+// start server
 server.listen(3000, () => {
   console.log("The server is running on port 3000")
 })
