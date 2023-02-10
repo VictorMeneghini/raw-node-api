e7b38cfc94c3489f1877a941238dea6f6c1cba24
VictorMeneghini <vhmeneghini@gmail.com>
2023-02-10 12:30:40 +0000

    feat: generate ssl/tls certificate to use https requests

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Dependencies
 const http = require("http")
+const https = require("https")
+const fs = require("fs")
+const nodePath = require("path")
 const { StringDecoder } = require("string_decoder")
 const url = require("url")
 const config = require("./config")
@@ -66,6 +69,67 @@ const server = http.createServer((req, res) => {
 
 })
 
+const httpsOptions = {
+  key: fs.readFileSync(nodePath.join(__dirname, "https", "key.pem")),
+  cert: fs.readFileSync(nodePath.join(__dirname, "https", "cert.pem")),
+}
+
+const httpsServer = https.createServer(httpsOptions, (req, res) => {
+  
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "")
+
+  // Get the querystring as an object
+  const queryStringObject = parsedUrl.query
+  // console.log(queryStringObject)
+
+  // Get HTTP method
+  const method = req.method.toLowerCase()
+  // console.log(method)
+
+  // Get the paylaod
+  const decoder = new StringDecoder("utf-8")
+  let buffer = ''
+
+  // Get the headers
+  const headers = req.headers
+  // console.log(headers)
+
+  req.on("data", (data) => {
+    buffer += decoder.write(data)
+  })
+
+  req.on("end", () => {
+    buffer += decoder.end()
+
+    const chosenHandler = router[trimmedPath] ? router[trimmedPath] : handlers.notFound
+
+    // contruct data object
+
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: buffer
+    }
+
+    // route request
+    chosenHandler(data, (statusCode, payload) => {
+      
+      res.setHeader("Content-Type", "application-json")
+      res.writeHead(statusCode)
+      res.end(JSON.stringify(payload))
+
+    })
+
+  })
+})
+
 // handlers 
 const handlers = {
   sample: (data, cb) => {
@@ -89,6 +153,6 @@ handlers.sample("my data", (statusCode) => {
 })
 
 // start server
-server.listen(config.port, () => {
-  console.log(`The server is running on port ${config.port}`)
+httpsServer.listen(config.httpsPort, () => {
+  console.log(`The server is running on port ${config.httpsPort}`)
 })
